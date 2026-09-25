5dd0b5112689333636ab8f94aeca85271a0e57f2
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-09-25 11:16:15 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen justify-center bg-slate-900 flex items-center">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        Tabish CSS v4 Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
